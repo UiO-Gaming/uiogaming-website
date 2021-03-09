276461cb6acda6a04c42812f9d4fda53ba53cc4a
LBlend <lblend@protonmail.com>
2021-03-10 00:22:51 +0100

    prettier

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -1,66 +1,68 @@
 export default {
-  name: 'post',
-  title: 'Blogginnlegg',
-  type: 'document',
+  name: "post",
+  title: "Blogginnlegg",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Tittel',
-      type: 'string',
+      name: "title",
+      title: "Tittel",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      description: 'Lenke til innlegget. Sørg for at tittelen er unik og trykk på generate. Hvis ikke, endre denne til noe unikt',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      description:
+        "Lenke til innlegget. Sørg for at tittelen er unik og trykk på generate. Hvis ikke, endre denne til noe unikt",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
       },
     },
     {
-      name: 'author',
-      title: 'Person',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "author",
+      title: "Person",
+      type: "reference",
+      to: { type: "author" },
     },
     {
-      name: 'mainImage',
-      title: 'Forsidebilde',
-      type: 'image',
+      name: "mainImage",
+      title: "Forsidebilde",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
     {
-      name: 'publishedAt',
-      title: 'Publiseringsdato',
-      type: 'datetime',
+      name: "publishedAt",
+      title: "Publiseringsdato",
+      type: "datetime",
     },
     {
-      name: 'body',
-      title: 'Brødtekst',
-      type: 'blockContent',
+      name: "body",
+      title: "Brødtekst",
+      type: "blockContent",
     },
     {
-      name: 'excerpt',
-      title: 'Sammendrag',
-      description: 'Dette er teksten som vil vises på forsiden som forhåndsvisning',
-      type: 'string',
+      name: "excerpt",
+      title: "Sammendrag",
+      description:
+        "Dette er teksten som vil vises på forsiden som forhåndsvisning",
+      type: "string",
     },
   ],
 
   preview: {
     select: {
-      title: 'title',
-      person: 'person.name',
-      media: 'mainImage',
+      title: "title",
+      person: "person.name",
+      media: "mainImage",
     },
     prepare(selection) {
-      const {person} = selection
+      const { person } = selection;
       return Object.assign({}, selection, {
         subtitle: person && `by ${person}`,
-      })
+      });
     },
   },
-}
+};
